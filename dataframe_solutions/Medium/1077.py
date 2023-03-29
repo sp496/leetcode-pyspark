24ef6cfe1868a5478196e561ceb449e6abea9b76
psicktrick1 <poojarysaurabh1996@gmail.com>
2023-03-29 14:42:33 +0530

    added 1070.py

---
+++ dataframe_solutions/Medium/1077.py
@@ -0,0 +1,31 @@
+from dependencies import spark_pg_utils
+
+
+def solution_1(spark):
+    # Question link
+    # https://www.jiakaobo.com/leetcode/1077.%20Project%20Employees%20III.htmll
+
+    # pyspark code
+
+    import pyspark.sql.functions as F
+    from pyspark.sql.window import Window
+
+    project_df = spark.read_table_as_df("project_1077")
+    project_df.show()
+
+    emp_df = spark.read_table_as_df("employee_1077")
+    emp_df.show()
+
+    w = Window.partitionBy('project_id').orderBy(F.desc('experience_years'))
+
+    result_df = project_df \
+        .join(emp_df, on='employee_id', how='inner') \
+        .withColumn('exp_rank', F.rank().over(w)) \
+        .filter(F.col('exp_rank') == 1) \
+        .select(['project_id', 'employee_id'])
+
+    result_df.show()
+
+
+if __name__ == '__main__':
+    spark_pg_utils.execute(solution_1)
