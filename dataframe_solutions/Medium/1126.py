f0340f78c5ad5e6de269fb92ed9576119d567f19
psicktrick1 <poojarysaurabh1996@gmail.com>
2023-03-30 12:39:19 +0530

    added 1126.py

---
+++ dataframe_solutions/Medium/1126.py
@@ -0,0 +1,29 @@
+from dependencies import spark_pg_utils
+
+
+def solution_1(spark):
+    # Question link
+    # https://www.jiakaobo.com/leetcode/1126.%20Active%20Businesses.html
+
+    # pyspark code
+
+    import pyspark.sql.functions as F
+    from pyspark.sql.window import Window
+
+    events_df = spark.read_table_as_df("events_1126")
+    events_df.show()
+
+    w = Window.partitionBy('event_type')
+
+    result_df = events_df \
+        .withColumn('avg_event_occurence', F.avg('occurences').over(w)) \
+        .filter(F.col('occurences') > F.col('avg_event_occurence')) \
+        .groupby('business_id').agg(F.count('event_type').alias('event_types')) \
+        .filter(F.col('event_types') >= 2) \
+        .select('business_id')
+
+    result_df.show()
+
+
+if __name__ == '__main__':
+    spark_pg_utils.execute(solution_1)
--- dataframe_solutions/solutions.md
+++ dataframe_solutions/solutions.md
@@ -414,10 +414,25 @@ result_df = enrol_df \
 result_df.show()
 ```
 
-### 
+### [1126. Active Businesses](https://www.jiakaobo.com/leetcode/1126.%20Active%20Businesses.html)
 
 ```python
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
 
+events_df = spark.read_table_as_df("events_1126")
+events_df.show()
+
+w = Window.partitionBy('event_type')
+
+result_df = events_df \
+    .withColumn('avg_event_occurence', F.avg('occurences').over(w)) \
+    .filter(F.col('occurences') > F.col('avg_event_occurence')) \
+    .groupby('business_id').agg(F.count('event_type').alias('event_types')) \
+    .filter(F.col('event_types') >= 2) \
+    .select('business_id')
+
+result_df.show()
 ```
 
 ### 
