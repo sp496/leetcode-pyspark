81333bd0c8803e131c8999ecac99268d68931685
Saurabh <poojarysaurabh1996@gmail.com>
2023-11-11 19:05:32 +0530

    updated solutions

--- dataframe_solutions/Medium/1164.py
+++ dataframe_solutions/Medium/1164.py
@@ -49,6 +49,21 @@ def solution_2(spark):
 
     result_df.show()
 
+def solution_3(spark):
+
+    from pyspark.sql import functions as F, Window as W
+
+    prod_df = spark.read_table_as_df("products_1164")
+    prod_df.show()
+
+    wspec = W.partitionBy('product_id').orderBy(F.desc('change_date')).rowsBetween(-1, W.currentRow)
+
+    result_df = prod_df \
+        .withColumn('price', F.when(F.col('change_date') <= '2019-08-16', F.col('new_price'))
+                    .otherwise(F.first('new_price').over(wspec))) \
+
+    result_df.show()
+
 
 if __name__ == '__main__':
-    spark_pg_utils.execute(solution_2)
+    spark_pg_utils.execute(solution_3)
