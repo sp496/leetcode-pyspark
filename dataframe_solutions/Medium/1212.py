87df54c5432ddaef80b20624813402883e5175dc
Saurabh <poojarysaurabh1996@gmail.com>
2023-11-13 12:55:42 +0530

    updated solutions

--- dataframe_solutions/Medium/1212.py
+++ dataframe_solutions/Medium/1212.py
@@ -4,14 +4,16 @@ from dependencies import spark_pg_utils
 def solution_1(spark):
 
     import pyspark.sql.functions as F
+
     t_df = spark.read_table_as_df("teams_1212")
     t_df.show()
+
     m_df = spark.read_table_as_df("matches_1212")
     m_df.show()
 
-    result_df = m_df.alias('m') \
-                .join(t_df.alias('t'), on=(F.col('m.host_team')==F.col('t.team_id')) |
-                                          (F.col('m.guest_team')==F.col('t.team_id')), how='left') \
+    result_df = t_df.alias('t') \
+                .join(m_df.alias('m'), on=(F.col('t.team_id')==F.col('m.host_team')) |
+                                          (F.col('t.team_id')==F.col('m.guest_team')), how='left') \
                 .groupby('team_id', 'team_name') \
                 .agg(F.sum(F.when((F.col('team_id') == F.col('host_team')) &
                                   (F.col('host_goals') > F.col('guest_goals')), 3)
@@ -22,6 +24,7 @@ def solution_1(spark):
                             .when((F.col('team_id') == F.col('guest_team'))
                                   & (F.col('guest_goals') == F.col('host_goals')), 1)
                             .otherwise(0)).alias('num_points')) \
+                .orderBy(F.desc('num_points'), F.asc('team_name'))
 
     result_df.show()
 
