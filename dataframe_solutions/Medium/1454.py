b236f698c1ac78e5e818e565f9162ca373e35337
Saurabh <poojarysaurabh1996@gmail.com>
2024-04-27 19:13:05 +0530

    updated solutions

--- dataframe_solutions/Medium/1454.py
+++ dataframe_solutions/Medium/1454.py
@@ -14,16 +14,15 @@ def solution_1(spark):
     wspec = W.partitionBy('id').orderBy('login_date')
 
     result_df = l_df \
-            .withColumn('days_since_last_login', (F.col('login_date') - F.lag('login_date', 1).over(wspec))
-                                .cast('int')) \
-            .fillna(1, 'days_since_last_login') \
-            .groupby('id').agg(F.count(F.when(F.col('days_since_last_login') == 1, True)).alias('consecutive_logins')) \
-            .filter(F.col('consecutive_logins') >= 5) \
-            .join(a_df, on='id') \
-            .select('id', 'name')
+        .dropDuplicates() \
+        .withColumn('rnk', F.row_number().over(wspec)) \
+        .withColumn('prev_date', F.col('login_date') - F.col('rnk')) \
+        .groupby('id', 'prev_date').agg(F.count('*').alias('consecutive_logins')) \
+        .filter(F.col('consecutive_logins') >= 5) \
+        .join(a_df, on='id') \
+        .select('id', 'name')
 
     result_df.show()
-
 
 def solution_2(spark):
 
@@ -38,13 +37,13 @@ def solution_2(spark):
     wspec = W.partitionBy('id').orderBy('login_date')
 
     result_df = l_df \
-        .dropDuplicates() \
-        .withColumn('rnk', F.row_number().over(wspec)) \
-        .withColumn('prev_date', F.col('login_date') - F.col('rnk')) \
-        .groupby('id', 'prev_date').agg(F.count('*').alias('consecutive_logins')) \
-        .filter(F.col('consecutive_logins') >= 5) \
-        .join(a_df, on='id') \
-        .select('id', 'name')
+            .withColumn('days_since_last_login', (F.col('login_date') - F.lag('login_date', 1).over(wspec))
+                                .cast('int')) \
+            .fillna(1, 'days_since_last_login') \
+            # .groupby('id').agg(F.count(F.when(F.col('days_since_last_login') == 1, True)).alias('consecutive_logins')) \
+            # .filter(F.col('consecutive_logins') >= 5) \
+            # .join(a_df, on='id') \
+            # .select('id', 'name')
 
     result_df.show()
 
